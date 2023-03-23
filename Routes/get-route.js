d56001cdf05b6ba7172190d80c472d3d5203cacd
Lucas Antunes <lucasssantunes@hotmail.com>
2023-03-23 17:27:45 -0300

    Get Request Error

--- Routes/get-route.js
+++ Routes/get-route.js
@@ -1,6 +1,6 @@
 const express = require("express");
 ticketsRouter = express.Router();
-
+const filmsArray = require("./films-tickets/films.js");
 
 let films = ["avatar", "avengers", "moana"];
 
@@ -8,6 +8,9 @@ ticketsRouter.get("/", (req, res, next) => {
    res.send(tickets);
 });
 
-ticketsRouter.get("/:id")
+ticketsRouter.get("/:id", (req, res, next) => {
+        let filmIndex  = filmsArray.indexOf( target => target.id == req.filmId);
+        res.status(200).send(filmsArray[filmIndex]);    
+})
 
 module.exports = ticketsRouter;

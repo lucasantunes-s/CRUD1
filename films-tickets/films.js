a32844c71051dcb07203d9901e391f7d0f40bc52
Lucas Antunes <lucasssantunes@hotmail.com>
2023-03-23 17:29:13 -0300

    Get Films Error v2

--- films-tickets/films.js
+++ films-tickets/films.js
@@ -1,3 +1,25 @@
 class Film {
-    constructor (id, name, )
+    constructor (id, name, runtime) {
+        this.id = id;
+        this.name = name;
+        this.runtime = runtime;
+    }
+
+    getId() {
+        return this.id;
+    }
+
+    getName() {
+        return this.name;
+    }
+
+    getRuntime() {
+        return this.runtime;
+    }
 }
+
+const avatar = new Film(1, "Avatar", 150);
+
+filmsArray = [avatar];
+
+module.exports = filmsArray;
--- server.js
+++ server.js
@@ -2,19 +2,31 @@ const express = require("express");
 const app = express();
 const PORT = 4000;
 const ticketsRouter = require("./Routes/get-route.js");
-const filmsArray = require("./Routes/get-route.js")
+const filmsArray = require("./films-tickets/films.js");
+
+
+app.param("filmId", (req, res, next, id) => {
+    let filmId = Number(filmId);
+    try {
+        const found = filmsArray.find(((target) => {
+            return filmId === target.id;
+        }))
+        if (found) {
+            req.filmId = found;
+            next();
+        } else {
+            next(new Error("Your film does not exist"));
+        };
+        
+    } catch (err) {
+        next (err)
+    }
+});
 
 
 app.use("/tickets", ticketsRouter);
 
-app.param("filmId", (req, res, next, id) => {
-    let filmName = String(filmName);
-    try {
-        const found = filmsArray.find(target => target === filmName)
-    }
-})
-
-
+app.use("/tickets/:filmId", ticketsRouter)
 
 
 
